036f9074b583a566450cbbd91dd8283ec592c9e3
root <root@Vinith>
2024-09-22 19:47:44 -0400

    Python to fetch remote data

---
+++ my-work/github-event.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+
+# In order to work with env variables and remote APIs you need three imports:
+import os
+import json
+import requests
+
+#To retrieve the value of an environment variable in Python, use this syntax:
+GHUSER = os.getenv('GITHUB_USER')
+#print(GHUSER)
+
+url = 'https://api.github.com/users/{0}/events'.format(GHUSER)
+
+#print(url)
+
+r = json.loads(requests.get(url).text)
+
+for x in r[:5]:
+  event = x['type'] + ' :: ' + x['repo']['name']
+  print(event)
+
+print(r)
